e74f4501a85ee097f2678e57dfc7190e9e7dc9b7
dam20 <46676174+dam20@users.noreply.github.com>
2020-07-24 01:04:17 -0300

    Update instala1.sh

--- instala1.sh
+++ instala1.sh
@@ -240,9 +240,7 @@ if [[ -e $HOME/lista-arq ]] && [[ ! $(cat $HOME/lista-arq|grep "KEY INVALIDA!") 
    for arqx in $(cat $HOME/lista-arq); do
    msg -verm "${stopping}${pontos}"
    #wget -O ${SCPinstal}/${arqx} ${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
-   
-   echo $arqx
-   
+   echo "archivo"
    wget --no-check-certificate -O ${SCPinstal}/${arqx} ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
    tput cuu1 && tput dl1
    pontos+="."
